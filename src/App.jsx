e6dbebf6e47d391d9cf54f26c22a3707b7a21d12
Eranda Dias <eranda.dias@azidsad.local>
2024-05-16 18:54:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from "./Components/Navbar"
+import Newsboard from "./Components/Newsboard"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Newsboard/>
+    </div>
+  )
+}
+
+export default App
+
+
